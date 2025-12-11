0b8c062c44844d47e191bc83295d0fe1616c9b62
madhuchalumuri <chalumurimadhu9999@gmail.com>
2025-12-11 09:41:27 +0530

    changed to dynamic api

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -1,2 +1,8 @@
+const { hostname, port } = window.location;
+
+// Always use HTTPS
+export const baseURL = `https://${hostname}${
+  port ? `:${port}` : ""
+}/License/api/v1/`;
 // export const baseURL = "http://localhost:8081/License/api/v1/";
-export const baseURL = "http://192.168.86.132:8181/License/api/v1/";
+// export const baseURL = "http://192.168.86.132:8181/License/api/v1/";
